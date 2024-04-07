faf1def4149ed069709ca8c65cec302e0dbc40a8
josilee <164757997+josilee@users.noreply.github.com>
2024-04-07 17:58:01 -0400

    Add files via upload

---
+++ SDEVm03lab.py
@@ -0,0 +1,39 @@
+class Vehicle:
+    def __init__(self, vehicle_type):
+        self.vehicle_type = vehicle_type
+
+class Automobile(Vehicle):
+    def __init__(self, vehicle_type, year, make, model, doors, roof):
+        super().__init__(vehicle_type)
+        self.year = year
+        self.make = make
+        self.model = model
+        self.doors = doors
+        self.roof = roof
+    
+    def display_info(self):
+        print(f"Vehicle type: {self.vehicle_type}")
+        print(f"Year: {self.year}")
+        print(f"Make: {self.make}")
+        print(f"Model: {self.model}")
+        print(f"Number of doors: {self.doors}")
+        print(f"Type of roof: {self.roof}")
+
+def main():
+    print("Please enter your car's details.")
+
+    vehicle_type = 'car' 
+    year = input("Year: ")
+    make = input("Make: ")
+    model = input("Model: ")
+    doors = input("Number of doors (2 or 4): ")
+    roof = input("Type of roof (solid or sun roof): ")
+
+    car = Automobile(vehicle_type, year, make, model, doors, roof)
+    
+    # Display car information
+    print("\nYour car's information:")
+    car.display_info()
+
+if __name__ == "__main__":
+    main()
